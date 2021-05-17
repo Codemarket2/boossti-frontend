e58e5469ed4b551e84ee8b5a35bdd04d206ff225
Vivek Thakur <vivek78643@gmail.com>
2021-05-17 13:52:10 +0530

    web screen

--- packages/web/src/screens/OnboardingScreen.tsx
+++ packages/web/src/screens/OnboardingScreen.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useOnBoarding } from '@frontend/shared/hooks/onBoarding';
-import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
+import Step1 from '../components/on-boarding/Step1';
+import Step2 from '../components/on-boarding/Step2';
+import Step3 from '../components/on-boarding/Step3';
 
 export default function OnBoarding() {
   const { state, handleContinue, handleSelectRole } = useOnBoarding();
