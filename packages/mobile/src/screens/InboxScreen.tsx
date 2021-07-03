a17a897f7d425b65eeef3adce71a62100d30745b
Vivek Thakur <vivek78643@gmail.com>
2021-07-03 16:19:53 +0530

    ios notification

--- packages/mobile/src/screens/InboxScreen.tsx
+++ packages/mobile/src/screens/InboxScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Title } from 'react-native-paper';
+import { Title, Button } from 'react-native-paper';
 import Screen from '../components/common/Screen';
+import { useSubscription } from '@frontend/shared/hooks/subscription';
+import { onAlert } from '../utils/alert';
 
 export default function InboxScreen() {
+  const { handleSendNotification, loading } = useSubscription({ onAlert });
   return (
     <Screen>
-      <Title>Inbox Screen</Title>
+      <Title>Click on this subscribe button so that you get a notification</Title>
+      <Button
+        mode="contained"
+        loading={loading}
+        disabled={loading}
+        onPress={handleSendNotification}>
+        Subscribe
+      </Button>
     </Screen>
   );
 }
--- packages/shared/graphql/index.ts
+++ packages/shared/graphql/index.ts
@@ -25,7 +25,7 @@ const jwtToken = async () => {
 const authLink = setContext(async (_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = await jwtToken();
-  console.log(token);
+  // console.log(token);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
