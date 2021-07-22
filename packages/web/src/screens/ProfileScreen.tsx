f70e9aad3c1c3f1622704e233ce5386696640ae4
Vivek Thakur <vivek78643@gmail.com>
2021-07-22 22:59:22 +0530

    bug fix

--- packages/web/src/screens/ProfileScreen.tsx
+++ packages/web/src/screens/ProfileScreen.tsx
@@ -4,14 +4,14 @@ import UserProfile from '../components/user/UserProfile';
 import GuestProfile from '../components/user/GuestProfile';
 
 export default function Page({ _id }: any) {
-  const { authenticated, attributes,initial } = useSelector(({ auth }: any) => auth);
-  const isCurrentUser = authenticated && attributes.['custom:_id'] === _id;
-  if(initial){
-  return (
-    <UserLayout>
-      {isCurrentUser ? <UserProfile /> : _id && <GuestProfile _id={_id} />}
-    </UserLayout>
-  );
-}
-  return <p>ot initalise</p>
+  const { authenticated, attributes, initial } = useSelector(({ auth }: any) => auth);
+
+  const isCurrentUser = authenticated && attributes['custom:_id'] === _id;
+
+  if (initial) {
+    return (
+      <UserLayout>{isCurrentUser ? <UserProfile /> : _id && <GuestProfile _id={_id} />}</UserLayout>
+    );
+  }
+  return null;
 }
