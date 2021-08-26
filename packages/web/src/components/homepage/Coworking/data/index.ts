1a5a11d4daf2272e5d7d1bde23e9112d1a48cb18
muzzamil <muzzamilshaikh40@gmail.com>
2021-08-26 19:51:36 +0530

    create new homepage inside web

---
+++ packages/web/src/components/homepage/Coworking/data/index.ts
@@ -0,0 +1,536 @@
+export const partners = [
+  {
+    name: 'Airbnb',
+    logo: 'https://assets.maccarianagency.com/the-front/logos/airbnb.svg',
+  },
+  {
+    name: 'Coinbase',
+    logo: 'https://assets.maccarianagency.com/the-front/logos/coinbase.svg',
+  },
+  {
+    name: 'Dribbble',
+    logo: 'https://assets.maccarianagency.com/the-front/logos/dribbble.svg',
+  },
+  {
+    name: 'Instagram',
+    logo: 'https://assets.maccarianagency.com/the-front/logos/instagram.svg',
+  },
+  {
+    name: 'Netflix',
+    logo: 'https://assets.maccarianagency.com/the-front/logos/netflix.svg',
+  },
+  {
+    name: 'Pinterest',
+    logo: 'https://assets.maccarianagency.com/the-front/logos/pinterest.svg',
+  },
+];
+
+// const vt = require('../../../assets/images/custom/place3@2x.jpeg');
+
+export const advantages = [
+  {
+    icon: 'fas fa-users',
+    title: 'Verify doctors',
+    subtitle:
+      'Verify doctors about their past experince details in treating patients with symptoms like yours.',
+  },
+  {
+    icon: 'far fa-address-book',
+    title: 'Compare treatments',
+    subtitle: 'Compare treatment plans with other patients.',
+  },
+  {
+    icon: 'fab fa-angellist',
+    title: 'Improve health',
+    subtitle: 'Educate yourself about health and wellness to improve your wellbeing.',
+  },
+  {
+    icon: 'fas fa-phone-alt',
+    title: 'Community support',
+    subtitle: 'Build a community around yourself rather than doing it alone.',
+  },
+];
+
+export const locations = [
+  {
+    images: [
+      {
+        src: require('../../assets/images/custom/place1@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place1@2x.jpeg'),
+        alt: '...',
+      },
+      {
+        src: require('../../assets/images/custom/place2@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place2@2x.jpeg'),
+        alt: '...',
+      },
+      {
+        src: require('../../assets/images/custom/place3@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place3@2x.jpeg'),
+        alt: '...',
+      },
+    ],
+    title: 'Soho Square',
+    address: 'Via E. Gola 4, 20147 Milan, Italy',
+    price: '$980 / month',
+    reviews: [
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams@2x.jpg 2x',
+        },
+        authorName: 'Veronica Adams',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini@2x.jpg 2x',
+        },
+        authorName: 'Akachi Luccini',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith.jpg',
+          srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith@2x.jpg 2x',
+        },
+        authorName: 'Jack Smith',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson@2x.jpg 2x',
+        },
+        authorName: 'Kate Segelson',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson@2x.jpg 2x',
+        },
+        authorName: 'Alex Johnson',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/valeria-kogan.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/valeria-kogan@2x.jpg 2x',
+        },
+        authorName: 'Valeria Kogan',
+      },
+    ],
+    score: '5.0',
+    reviewCount: 28,
+  },
+  {
+    images: [
+      {
+        src: require('../../assets/images/custom/place1@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place1@2x.jpeg'),
+        alt: '...',
+      },
+      {
+        src: require('../../assets/images/custom/place2@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place2@2x.jpeg'),
+        alt: '...',
+      },
+      {
+        src: require('../../assets/images/custom/place3@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place3@2x.jpeg'),
+        alt: '...',
+      },
+    ],
+    title: 'Moose Lab',
+    address: 'Via E. Gola 4, 20147 Milan, Italy',
+    price: '$980 / month',
+    reviews: [
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams@2x.jpg 2x',
+        },
+        authorName: 'Veronica Adams',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini@2x.jpg 2x',
+        },
+        authorName: 'Akachi Luccini',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith.jpg',
+          srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith@2x.jpg 2x',
+        },
+        authorName: 'Jack Smith',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson@2x.jpg 2x',
+        },
+        authorName: 'Kate Segelson',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson@2x.jpg 2x',
+        },
+        authorName: 'Alex Johnson',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/valeria-kogan.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/valeria-kogan@2x.jpg 2x',
+        },
+        authorName: 'Valeria Kogan',
+      },
+    ],
+    score: '4.9',
+    reviewCount: 38,
+  },
+  {
+    images: [
+      {
+        src: require('../../assets/images/custom/place1@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place1@2x.jpeg'),
+        alt: '...',
+      },
+      {
+        src: require('../../assets/images/custom/place2@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place2@2x.jpeg'),
+        alt: '...',
+      },
+      {
+        src: require('../../assets/images/custom/place3@2x.jpeg'),
+        srcSet: require('../../assets/images/custom/place3@2x.jpeg'),
+        alt: '...',
+      },
+    ],
+    title: 'Tenoha Space',
+    address: 'Via E. Gola 4, 20147 Milan, Italy',
+    price: '$980 / month',
+    reviews: [
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams@2x.jpg 2x',
+        },
+        authorName: 'Veronica Adams',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini@2x.jpg 2x',
+        },
+        authorName: 'Akachi Luccini',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith.jpg',
+          srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith@2x.jpg 2x',
+        },
+        authorName: 'Jack Smith',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson@2x.jpg 2x',
+        },
+        authorName: 'Kate Segelson',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson@2x.jpg 2x',
+        },
+        authorName: 'Alex Johnson',
+      },
+      {
+        authorPhoto: {
+          src: 'https://assets.maccarianagency.com/the-front/photos/people/valeria-kogan.jpg',
+          srcSet:
+            'https://assets.maccarianagency.com/the-front/photos/people/valeria-kogan@2x.jpg 2x',
+        },
+        authorName: 'Valeria Kogan',
+      },
+    ],
+    score: '5.0',
+    reviewCount: 18,
+  },
+];
+
+export const properties = [
+  {
+    title: 'Questions to doctors',
+    icon: 'fas fa-phone-alt',
+  },
+  {
+    title: 'Compare treatments',
+    icon: 'fas fa-users',
+  },
+  {
+    title: 'Shared spaces',
+    icon: 'fas fa-couch',
+  },
+  {
+    title: 'Room cleaning',
+    icon: 'fas fa-pump-soap',
+  },
+  {
+    title: 'Gym membership',
+    icon: 'fas fa-dumbbell',
+  },
+  {
+    title: 'Super fast WiFi',
+    icon: 'fas fa-wifi',
+  },
+  {
+    title: 'Washing machines & dryers',
+    icon: 'fas fa-soap',
+  },
+  {
+    title: 'Cultural events programme',
+    icon: 'fas fa-calendar-week',
+  },
+];
+
+export const reviews = [
+  {
+    authorPhoto: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams@2x.jpg 2x',
+    },
+    authorName: 'Veronica Adams',
+    authorOccupation: 'Growth Marketer, Crealytics',
+    feedback:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    authorPhoto: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini@2x.jpg 2x',
+    },
+    authorName: 'Akachi Luccini',
+    authorOccupation: 'Lead Generation, Alternative Capital',
+    feedback:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    authorPhoto: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith@2x.jpg 2x',
+    },
+    authorName: 'Jack Smith',
+    authorOccupation: 'Head of Operations, Parkfield Commerce',
+    feedback:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
+
+export const events = [
+  {
+    cover: {
+      src: require('../../assets/images/custom/expo/gallery1@2x.jpeg'),
+      srcSet: require('../../assets/images/custom/expo/gallery1@2x.jpeg'),
+    },
+    title: 'Sales philosophy and future',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    place: 'Dojo Club',
+    time: '2:00 PM - 5:00 PM',
+  },
+  {
+    cover: {
+      src: require('../../assets/images/custom/expo/gallery2@2x.jpeg'),
+      srcSet: require('../../assets/images/custom/expo/gallery2@2x.jpeg'),
+    },
+    title: 'Introduction to Design Thinking',
+    description:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    place: 'Upstairs',
+    time: '11:30 AM - 1:00 AM',
+  },
+  {
+    cover: {
+      src: require('../../assets/images/custom/expo/gallery3@2x.jpeg'),
+      srcSet: require('../../assets/images/custom/expo/gallery3@2x.jpeg'),
+    },
+    title: 'Instagram grow experiments',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    place: 'Loop Lab',
+    time: '6:15 PM - 8:00 PM',
+  },
+];
+
+export const gallery = [
+  {
+    image: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/akachi-luccini@2x.jpg 2x',
+    },
+    title: 'gallery',
+    cols: 1,
+  },
+  {
+    image: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/alex-johnson@2x.jpg 2x',
+    },
+    title: 'gallery',
+    cols: 2,
+  },
+  {
+    image: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/jack-smith@2x.jpg 2x',
+    },
+    title: 'gallery',
+    cols: 1,
+  },
+  {
+    image: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/kate-segelson@2x.jpg 2x',
+    },
+    title: 'gallery',
+    cols: 1,
+  },
+  {
+    image: {
+      src: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams.jpg',
+      srcSet: 'https://assets.maccarianagency.com/the-front/photos/people/veronica-adams@2x.jpg 2x',
+    },
+    title: 'gallery',
+    cols: 1,
+  },
+];
+
+export const mapData = [
+  {
+    location: {
+      y: 45.453211,
+      x: 9.248383,
+      address: 'Via A.G. Alaimo 147, 55027',
+    },
+    phone: '0383 4586173',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.419211,
+      x: 9.021383,
+      address: 'Via Rocca de Baldi 95, 440368',
+    },
+    phone: '0358 4880742',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.473211,
+      x: 9.298383,
+      address: 'Via Firenze 134, 45588',
+    },
+    phone: '0315 1869381',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.461211,
+      x: 9.000383,
+      address: 'Via Cavour 29, 201558',
+    },
+    phone: '0387 5074352',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.413211,
+      x: 9.398383,
+      address: 'Via Bologna 33, 220156',
+    },
+    phone: '0315 1869985',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.569211,
+      x: 9.128383,
+      address: 'Vicolo Tre Marchetti 127, 350125',
+    },
+    phone: '0352 9890835',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.483211,
+      x: 9.148383,
+      address: 'Via Lombardi 146, 45830',
+    },
+    phone: '0379 1768288',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+  {
+    location: {
+      y: 45.313211,
+      x: 9.012383,
+      address: 'Via Guantai Nuovi 99, 56989',
+    },
+    phone: '0379 1768849',
+    products: [
+      'https://assets.maccarianagency.com/the-front/photos/coworking/coworking-main-hero.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place1.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place2.jpg',
+      'https://assets.maccarianagency.com/the-front/photos/coworking/place3.jpg',
+    ],
+  },
+];
---
+++ packages/web/src/components/homepage/components/organisms/HeroSimpleBackground/HeroSimpleBackground.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import { Section } from '../../organisms';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+
+const HeroSimpleBackground = ({
+  children,
+  backgroundSize = 'cover',
+  backgroundImage,
+  backgroundPosition = 'center',
+  className,
+  ...rest
+}: HeroSimpleBackgroundProps): JSX.Element => {
+  const classes = useStyles();
+
+  const useBackground = makeStyles(() => ({
+    backgroundImage: {
+      backgroundImage: `url(${backgroundImage})`,
+    },
+    backgroundSize: {
+      backgroundSize: backgroundSize,
+    },
+    backgroundPosition: {
+      backgroundPosition: backgroundPosition,
+    },
+  }));
+  const backgroundClasses = useBackground();
+
+  return (
+    <div
+      className={clsx(
+        'hero-simple-background',
+        classes.root,
+        className,
+        backgroundClasses.backgroundImage,
+        backgroundClasses.backgroundSize,
+        backgroundClasses.backgroundPosition,
+      )}
+      {...rest}>
+      <Section className="hero-simple-background__section">{children}</Section>
+    </div>
+  );
+};
+
+export default HeroSimpleBackground;
