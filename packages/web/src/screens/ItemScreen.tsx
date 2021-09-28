1fd869c5087f05b85c8284dafd2f676ca5244910
Vivek Thakur <vivek78643@gmail.com>
2021-09-28 21:47:28 +0530

    scroll bug fix

--- packages/web/src/screens/ItemScreen.tsx
+++ packages/web/src/screens/ItemScreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import UserLayout from '../components/common/UserLayout';
 import ItemScreen from '../components/list/ItemScreen';
@@ -8,14 +9,32 @@ interface IProps {
 }
 
 export default function Screen({ slug, typeSlug }: IProps) {
+  const [state, setState] = useState('');
   const router = useRouter();
   const onSlugUpdate = (newSlug) => {
     router.push(`/types/${typeSlug}/${newSlug}`);
   };
+
+  const pushToAnchor = () => {
+    if (router.asPath.includes('#')) {
+      if (state !== router.asPath) {
+        setState(router.asPath);
+        setTimeout(() => {
+          router.push(router.asPath);
+        }, 1500);
+      }
+    }
+  };
+
   return (
     <UserLayout container={false} authRequired>
       <div className="px-3">
-        <ItemScreen onSlugUpdate={onSlugUpdate} slug={slug} typeSlug={typeSlug} />
+        <ItemScreen
+          onSlugUpdate={onSlugUpdate}
+          pushToAnchor={pushToAnchor}
+          slug={slug}
+          typeSlug={typeSlug}
+        />
       </div>
     </UserLayout>
   );
