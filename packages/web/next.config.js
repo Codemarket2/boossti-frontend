9dcf6e0e4cb29125f4b82030a434243f46a88433
muzzamil <muzzamilshaikh40@gmail.com>
2021-10-14 15:51:54 +0530

    edit next.confiq.js

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,9 +2,12 @@ const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa')({
   pwa: {
-    dest: 'public',
+    // dest: 'public',
+    // register: true,
+    // skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
     register: true,
-    skipWaiting: true,
+    sw: '/sw.js',
   },
 });
 
