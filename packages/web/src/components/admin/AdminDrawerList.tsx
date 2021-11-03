49b7e093f3bf41148780b39cca9913f356e4b9c1
muzzamil <muzzamilshaikh40@gmail.com>
2021-11-03 20:03:58 +0530

    Implemented logo upload feature

--- packages/web/src/components/admin/AdminDrawerList.tsx
+++ packages/web/src/components/admin/AdminDrawerList.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import GroupIcon from '@material-ui/icons/Group';
 import ListIcon from '@material-ui/icons/List';
 import { useRouter } from 'next/router';
@@ -8,6 +11,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Link from 'next/link';
+
+import ImageUpload from '../common/ImageUpload';
 
 const checkActiveRoute = (activeRoute, linkPathname) => {
   return activeRoute.pathname === linkPathname;
@@ -49,6 +54,22 @@ export default function AdminDrawerList() {
           <ListItemText primary="Types" />
         </ListItem>
       </Link>
+
+      <ImageUpload name="Logo Upload" uploadId="upload-logo" />
+      <ImageUpload name="Fav Icon" uploadId="upload-fav" />
+      <ImageUpload name="Menu Logo Upload" uploadId="Menu-icon" />
+      <Container style={{ display: 'flex', flexDirection: 'column' }}>
+        <TextField
+          multiline
+          rows={4}
+          id="outlined-basic"
+          label="App Description"
+          variant="outlined"
+        />
+        <Button color="primary" variant="contained">
+          submit
+        </Button>
+      </Container>
       <Divider />
     </>
   );
