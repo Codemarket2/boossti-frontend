b49f3a3ec24fc4ec2e1add6877979259b8adc2de
Vivek Thakur <vivek78643@gmail.com>
2021-11-20 20:30:03 +0530

    serverless nextjs version

--- packages/web/pages/layout.tsx
+++ packages/web/pages/layout.tsx
@@ -1,43 +1,107 @@
-import GridLayout, { Responsive as ResponsiveGridLayout } from 'react-grid-layout';
+import React from 'react';
+import { WidthProvider, Responsive } from 'react-grid-layout';
 
-export default function page() {
-  const layout = [
-    { i: 'a', x: 0, y: 0, w: 6, h: 1 },
-    { i: 'b', x: 0, y: 0, w: 6, h: 1 },
-  ];
-  //   return (
-  //     <ResponsiveGridLayout
-  //       className="layout"
-  //       layouts={layout}
-  //       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-  //       cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-  //     >
-  //       <div key="1" className="bg-danger">
-  //         1
-  //       </div>
-  //       <div key="2" className="bg-danger">
-  //         2
-  //       </div>
-  //       <div key="3" className="bg-danger">
-  //         3
-  //       </div>
-  //     </ResponsiveGridLayout>
-  //   );
-  return (
-    <GridLayout
-      className="layout"
-      layout={layout}
-      cols={12}
-      //   rowHeight={30}
-      width={1000}
-      onLayoutChange={(newLayout) => console.log('newLayout', newLayout)}
-    >
-      <div key="a" className="bg-danger">
-        a
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+const originalLayouts: any = getFromLS('layouts') || {
+  lg: [
+    { i: '1', w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 1 },
+    { i: '2', w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 1 },
+  ],
+};
+
+/**
+ * This layout demonstrates how to sync multiple responsive layouts to localstorage.
+ */
+export default class ResponsiveLocalStorageLayout extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   layouts: JSON.parse(JSON.stringify(originalLayouts)),
+    // };
+  }
+
+  state: any = {
+    layouts: JSON.parse(JSON.stringify(originalLayouts)),
+  };
+
+  static get defaultProps() {
+    return {
+      className: 'layout',
+      cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+      rowHeight: 30,
+    };
+  }
+
+  resetLayout() {
+    this.setState({ layouts: {} });
+  }
+
+  onLayoutChange(layout, layouts) {
+    console.log({ layouts });
+    saveToLS('layouts', layouts);
+    this.setState({ layouts });
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={() => this.resetLayout()}>
+          Reset Layout
+        </button>
+        <ResponsiveReactGridLayout
+          className="layout"
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          rowHeight={30}
+          layouts={this.state?.layouts}
+          onLayoutChange={(layout, layouts) => this.onLayoutChange(layout, layouts)}
+        >
+          <div
+            className="bg-danger"
+            key="1"
+            // data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 1 }}
+          >
+            <span className="text">1</span>
+          </div>
+          <div
+            className="bg-primary"
+            key="2"
+            // data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 1 }}
+          >
+            <span className="text">1</span>
+          </div>
+          <div
+            className="bg-primary"
+            key="3"
+            // data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 1 }}
+          >
+            <span className="text">1</span>
+          </div>
+        </ResponsiveReactGridLayout>
       </div>
-      <div key="b" className="bg-warning">
-        b
-      </div>
-    </GridLayout>
-  );
+    );
+  }
+}
+
+function getFromLS(key) {
+  let ls = {};
+  if (global.localStorage) {
+    try {
+      ls = JSON.parse(global.localStorage.getItem('rgl-8')) || {};
+    } catch (e) {
+      /*Ignore*/
+    }
+  }
+  return ls[key];
+}
+
+function saveToLS(key, value) {
+  if (global.localStorage) {
+    global.localStorage.setItem(
+      'rgl-8',
+      JSON.stringify({
+        [key]: value,
+      }),
+    );
+  }
 }
