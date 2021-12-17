67f002e821b09389642708d398f543f7fcc30b85
SonuKumar81800 <sonu.patna0808@gmail.com>
2021-12-17 13:26:34 +0530

    Modifing ActivityList

--- packages/web/src/components/activityLog/ActivityList.tsx
+++ packages/web/src/components/activityLog/ActivityList.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable react/jsx-wrap-multilines */
 import Typography from '@material-ui/core/Typography';
 import { useGetMyResponses } from '@frontend/shared/hooks/response';
-import { useEffect } from 'react';
 import Link from 'next/link';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Grid,
+} from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons';
 
 export default function ActivityList() {
   const { data, error, loading, state, setState } = useGetMyResponses();
 
   console.log(data?.getMyResponses);
-  // console.log(getMyResponses?.count);
 
   return (
     <>
@@ -19,26 +26,43 @@ export default function ActivityList() {
         </>
       ) : (
         <>
-          <table>
-            <tr>
-              <th>Form Id</th>
-              <th>Created By</th>
-              <th>Edit Link</th>
-            </tr>
-            {data?.getMyResponses?.data?.map((d) => (
-              <tr>
-                <td>{d?.formId}</td>
-                <td>{d?.createdBy?.name}</td>
-                <td>
-                  <Link href={`response/${d?._id}`}>
-                    <a> Edit </a>
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </table>
+          {data?.getMyResponses?.data?.map((d) => (
+            <ActivityAccordion data={d} />
+          ))}
         </>
       )}
     </>
+  );
+}
+
+function ActivityAccordion({ data }) {
+  console.log(data);
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMore />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{data?._id}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Box>
+            {data?.values?.map((v) => (
+              <Grid container spacing={2} key={v._id}>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                  <span style={{ margin: '0px 20px' }}>Field Id : {v.field} </span>{' '}
+                  <span>Value : {v.value}</span>
+                </Grid>
+              </Grid>
+            ))}
+            <Link href={`response/${data._id}`}>
+              <Button variant="outlined">Edit</Button>
+            </Link>
+          </Box>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 }
