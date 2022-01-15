e934e8b8d179b1e8c2453773d023dae8227a2840
Raj <75741866+rajdama@users.noreply.github.com>
2022-01-15 21:55:03 +0530

    mention added (#105)

--- packages/web/src/components/common/DisplayRichText.tsx
+++ packages/web/src/components/common/DisplayRichText.tsx
@@ -1,5 +1,8 @@
 import parse from 'html-react-parser';
 import styled from 'styled-components';
+import { client } from '@frontend/shared/graphql';
+import { useRouter } from 'next/router';
+import { GET_LIST_ITEM_BY_ID } from '@frontend/shared/graphql/query/list';
 
 type IProps = {
   value: string;
@@ -21,8 +24,47 @@ const StyledDiv = styled.div`
   }
 `;
 
-export default function DisplayRichText({ value = '' }: IProps) {
-  return <StyledDiv className="ck-content">{parse(transform(value))}</StyledDiv>;
+export default function DisplayRichText({ value = 'dd' }: IProps) {
+  const router = useRouter();
+  const getData = async (id) => {
+    const listItem = await getListItem(id);
+    router.push(`/page/${listItem['data'].getListItem.slug}`);
+  };
+  return (
+    <StyledDiv className="ck-content">
+      {parse(transform(value), {
+        replace: (domNode: any) => {
+          if (domNode.name == 'a' && domNode.attribs.class == 'mention') {
+            return (
+              <div
+                style={{ cursor: 'pointer', color: 'blue' }}
+                onClick={() => {
+                  getData(domNode.attribs['data-user-id']);
+                }}
+              >
+                {console.log(domNode)}
+                {domNode.attribs['data-mention']}
+              </div>
+            );
+          }
+        },
+      })}
+    </StyledDiv>
+  );
+}
+
+export async function getListItem(_id) {
+  try {
+    const response = await client.query({
+      query: GET_LIST_ITEM_BY_ID,
+      variables: { _id: _id },
+    });
+    return new Promise((resolve) => {
+      resolve(response);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 const transform = (htmlContent: any) => {
