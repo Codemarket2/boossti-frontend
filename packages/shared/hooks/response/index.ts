f1cfecfcad18e48405c9aa94b0d4fe9863d4474c
Sonu Kumar <45717030+SonuKumar81800@users.noreply.github.com>
2022-01-15 22:10:34 +0530

    Added  bulk upload response to form action (#104)
    * update response bug fix
    
    * Bulk Upload Feature implemented For Testing
    
    * Mutation For Bulk Responses
    
    * Bulk Upload Final test
    
    * Optimize Data Process logic in FrontEnd and Loading Button
    
    Co-authored-by: Vivek Thakur <vivek78643@gmail.com>

--- packages/shared/hooks/response/index.ts
+++ packages/shared/hooks/response/index.ts
@@ -2,3 +2,4 @@ export { useGetResponse, useGetResponses } from './getResponse';
 export { useCreateUpdateResponse } from './createUpdateResponse';
 export { useDeleteResponse } from './deleteResponse';
 export { useGetMyResponses } from './getMyResponses';
+export { useCreateBulkResponse } from './createBulkResponse';
