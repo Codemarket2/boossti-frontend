61801a11fb41826a1a50022eedd057cf5d3c4eeb
Raj <75741866+rajdama@users.noreply.github.com>
2022-01-25 12:42:12 +0530

    leader board added (#107)

--- packages/web/src/components/form2/FieldViewWrapper.tsx
+++ packages/web/src/components/form2/FieldViewWrapper.tsx
@@ -1,6 +1,11 @@
 import { useGetForm } from '@frontend/shared/hooks/form';
 import FormView from './FormView';
 import ErrorLoading from '../common/ErrorLoading';
+import Overlay from '../common/Overlay';
+import Box from '@material-ui/core/Box';
+import LinearProgress, { LinearProgressProps } from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import { useState } from 'react';
 
 interface IProps {
   _id: string;
@@ -9,6 +14,10 @@ interface IProps {
   customSettings: any;
 }
 
+const initialState = {
+  showForm: false,
+};
+
 export default function FieldViewWrapper({
   _id,
   parentId,
@@ -16,6 +25,16 @@ export default function FieldViewWrapper({
   customSettings,
 }: IProps): any {
   const { error, data } = useGetForm(_id);
+  const [state, setstate] = useState(initialState);
+  let settings;
+  console.log(customSettings);
+  if (customSettings) {
+    settings = customSettings;
+    console.log(111);
+  } else {
+    console.log(111);
+    settings = data?.getForm?.settings;
+  }
 
   if (error?.message?.includes("has coerced Null value for NonNull type 'ID!'")) {
     return null;
@@ -24,10 +43,52 @@ export default function FieldViewWrapper({
   if (error || !data || !data.getForm) {
     return <ErrorLoading error={error} />;
   }
+  if (settings.widgetType == 'button') {
+    return (
+      <>
+        <div className="text-center my-5">
+          <button
+            onClick={() => {
+              setstate({ ...state, showForm: true });
+            }}
+            className="btn btn-lg btn-primary"
+          >
+            {settings.widgetLabel}
+          </button>
+        </div>
+        <div>
+          {state.showForm && (
+            <Overlay
+              open={state.showForm}
+              title={`Form`}
+              onClose={() => {
+                setstate({ ...state, showForm: false });
+              }}
+            >
+              <div style={{ padding: '20px' }}>
+                <FormView
+                  form={{ ...data.getForm, settings: settings }}
+                  parentId={parentId}
+                  createCallback={createCallback}
+                />
+              </div>
+            </Overlay>
+          )}
+        </div>
+      </>
+    );
+  }
+
+  // if(settings.widgetType == 'leaderBoard'){
+
+  //   return(
+
+  //   )
+  // }
 
   return (
     <FormView
-      form={{ ...data.getForm, settings: customSettings ? customSettings : data.getForm.settings }}
+      form={{ ...data.getForm, settings: settings }}
       parentId={parentId}
       createCallback={createCallback}
     />
--- packages/web/src/components/form2/ResponseCount.tsx
+++ packages/web/src/components/form2/ResponseCount.tsx
@@ -4,21 +4,47 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
+import Box from '@material-ui/core/Box';
+import LinearProgress, { LinearProgressProps } from '@material-ui/core/LinearProgress';
 import { useState } from 'react';
 import ErrorLoading from '../common/ErrorLoading';
 import ResponseList from './ResponseList';
+import { useEffect } from 'react';
 
 interface IProps {
   formId: any;
   parentId?: string;
+  settings?: any;
 }
 
-export default function ResponseCount({ formId, parentId }: IProps): any {
+export default function ResponseCount({ formId, parentId, settings }: IProps): any {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const { data, error, loading } = useGetResponses(formId, parentId);
   const { data: formData, error: errorData } = useGetForm(formId);
+  let progress;
+  console.log(settings);
+  if (settings == undefined || settings.minValue == '') {
+    progress = 0;
+  } else {
+    progress = (data?.getResponses?.count / settings?.minValue) * 100;
+  }
+  useEffect(() => {
+    progress = (data?.getResponses?.count / settings?.minValue) * 100;
+  }, [data?.getResponses?.count]);
+  function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+      <Box style={{ display: 'flex', alignItems: 'center' }}>
+        <Box style={{ width: '100%' }}>
+          <LinearProgress variant="determinate" {...props} />
+        </Box>
+        <Box style={{ minWidth: 35 }}>
+          <Typography variant="body2">{`${Math.round(progress)}%`}</Typography>
+        </Box>
+      </Box>
+    );
+  }
 
   if (error?.message?.includes("has coerced Null value for NonNull type 'ID!'")) {
     return null;
@@ -31,9 +57,13 @@ export default function ResponseCount({ formId, parentId }: IProps): any {
   return (
     <>
       <div className="text-center mt-2">
-        <Button variant="outlined" onClick={handleOpen}>
-          {`${data?.getResponses?.count || 0} Responses`}
-        </Button>
+        {settings != null && settings?.minValue ? (
+          <LinearProgressWithLabel value={progress} />
+        ) : (
+          <Button variant="outlined" onClick={handleOpen}>
+            {`${data?.getResponses?.count || 0} Responses`}
+          </Button>
+        )}
       </div>
       {open && (
         <Dialog fullScreen open={open} onClose={handleClose}>
