f86f33b4f9169fa2c9628d6892ce14dfc4ab1b40
Vivek Thakur <vivek78643@gmail.com>
2022-03-09 17:33:28 +0530

    select form submit button text

--- packages/web/src/components/form2/FormView.tsx
+++ packages/web/src/components/form2/FormView.tsx
@@ -259,7 +259,7 @@ export default function FormViewWrapper({
                       }
                     }}
                   >
-                    Submit kdk
+                    Submit
                   </Button>
                 </>
               ) : (
