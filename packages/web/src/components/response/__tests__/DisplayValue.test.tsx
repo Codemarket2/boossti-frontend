c4dcaa9deca198b38ee978e8208015a95af1535b
arjavsethi <arjavsethi07@gmail.com>
2022-07-28 22:04:02 +0530

    v2

--- packages/web/src/components/response/__tests__/DisplayValue.test.tsx
+++ packages/web/src/components/response/__tests__/DisplayValue.test.tsx
@@ -2,45 +2,40 @@ import { useState } from 'react';
 import userEvent from '@testing-library/user-event';
 import { fireEvent, render, screen, act, waitFor } from '../../../../jest/test-utils';
 import DisplayValue from '../../form2/DisplayValue';
-
-const initialprops = {
-  field: {
-    fieldType: 'email',
-    form: null,
-    label: 'email',
-    options: {
-      caseInsensitiveUnique: false,
-      default: false,
-      formField: '',
-      multipleValues: false,
-      notEditable: false,
-      physicalQuantity: '',
-      required: true,
-      selectAllowCreate: false,
-      selectItem: false,
-      selectOptions: [''],
-      showAsCheckbox: false,
-      showCommentBox: false,
-      showOptionCreatedByUser: false,
-      showOptionCreatedOnTemplate: false,
-      showStarRating: false,
-      staticText: '',
-      systemCalculatedAndSaved: false,
-      systemCalculatedAndView: false,
-      systemValue: null,
-      unique: false,
-      unit: '',
+import { defaultOptions } from '../../../../../shared/hooks/form/addFields';
+// const initialprops = {
+//   field: {
+//     fieldType: 'email',
+//     form: null,
+//     label: 'email',
+//     options: {
+//       ...defaultOptions,
+//     },
+//   },
+//   value: { value: 'arjavsethi07@gmail.com' },
+//   imageAvatar: true,
+//   verticalView: true,
+//   _id: '62b0cf932b8e64fb467cfffc',
+//   template: null,
+// };
+const getInitialProps = () => {
+  return {
+    field: {
+      fieldType: 'email',
+      form: null,
+      label: 'email',
+      options: {
+        ...defaultOptions,
+      },
     },
-  },
-  value: { value: 'arjavsethi07@gmail.com' },
-  imageAvatar: true,
-  verticalView: true,
-  _id: '62b0cf932b8e64fb467cfffc',
-  template: null,
+    value: { value: 'arjavsethi07@gmail.com' },
+    imageAvatar: true,
+    verticalView: true,
+    _id: '62b0cf932b8e64fb467cfffc',
+    template: null,
+  };
 };
-
-const DisplayValueTest = () => {
-  const [props, setprops] = useState(initialprops);
+const DisplayValueTest = (props): any => {
   return (
     <>
       <DisplayValue
@@ -54,71 +49,72 @@ const DisplayValueTest = () => {
 };
 
 describe('checks the working of switch case', () => {
+  const x = getInitialProps();
   it('checks email rendering', () => {
-    render(<DisplayValueTest />);
+    render(<DisplayValueTest {...x} />);
     const email = screen.getByTestId('text-output');
     expect(email).toBeInTheDocument();
-    const value = initialprops.value.value;
+    const value = x.value.value;
     expect(email).toHaveTextContent(value);
   });
 
-  initialprops.field.fieldType = 'text';
-  initialprops.value.value = 'arjavsethi';
+  x.field.fieldType = 'text';
+  x.value.value = 'arjavsethi';
   it('checks text rendering', () => {
-    render(<DisplayValueTest />);
+    render(<DisplayValueTest {...x} />);
     const text = screen.getByTestId('text-output');
     expect(text).toBeInTheDocument();
-    const value = initialprops.value.value;
+    const value = x.value.value;
     expect(text).toHaveTextContent(value);
   });
 
-  initialprops.field.fieldType = 'textarea';
-  initialprops.value.value = 'arjavsethislebgfdkjdlesvfljusvfousvcykzjaushcvouseyf';
+  x.field.fieldType = 'textarea';
+  x.value.value = 'arjavsethislebgfdkjdlesvfljusvfousvcykzjaushcvouseyf';
   it('checks text area rendering', () => {
-    render(<DisplayValueTest />);
+    render(<DisplayValueTest {...x} />);
     const textArea = screen.getByTestId('text-output');
     expect(textArea).toBeInTheDocument();
-    const value = initialprops.value.value;
+    const value = x.value.value;
     expect(textArea).toHaveTextContent(value);
   });
 
-  initialprops.field.fieldType = 'url';
-  initialprops.value.value = 'https://www.google.com';
+  x.field.fieldType = 'url';
+  x.value.value = 'https://www.google.com';
   it('checks text area rendering', () => {
-    render(<DisplayValueTest />);
+    render(<DisplayValueTest {...x} />);
     const url = screen.getByTestId('text-output');
     expect(url).toBeInTheDocument();
-    const value = initialprops.value.value;
+    const value = x.value.value;
     expect(url).toHaveTextContent(value);
   });
 
-  initialprops.field.fieldType = 'password';
-  initialprops.value.value = 'arjavsethi';
+  x.field.fieldType = 'password';
+  x.value.value = 'arjavsethi';
   it('checks password rendering', () => {
-    render(<DisplayValueTest />);
+    render(<DisplayValueTest {...x} />);
     const password = screen.getByTestId('text-output');
     expect(password).toBeInTheDocument();
-    const value = initialprops.value.value;
+    const value = x.value.value;
     expect(password).toHaveTextContent(value);
   });
 
-  initialprops.field.fieldType = 'number';
-  initialprops.value.value = '12345';
-  it('checks number rendering', () => {
-    render(<DisplayValueTest />);
-    const number = screen.getByTestId('number-output');
-    expect(number).toBeInTheDocument();
-    const value = initialprops.value.value;
-    expect(number).toHaveTextContent(value);
-  });
+  // x.field.fieldType = 'number';
+  // x.value.value = '12345';
+  // it('checks number rendering', () => {
+  //   render(<DisplayValueTest {...x} />);
+  //   const number = screen.getByTestId('number-output');
+  //   expect(number).toBeInTheDocument();
+  //   const value = x.value.value;
+  //   expect(number).toHaveTextContent(value);
+  // });
 
-  initialprops.field.fieldType = 'date';
-  initialprops.value.value = '2020-01-01';
+  x.field.fieldType = 'date';
+  x.value.value = '2020-01-01';
   it('checks date rendering', () => {
-    render(<DisplayValueTest />);
+    render(<DisplayValueTest {...x} />);
     const date = screen.getByTestId('date-output');
     expect(date).toBeInTheDocument();
-    const value = initialprops.value.value;
+    const value = x.value.value;
     expect(date).toHaveTextContent(value);
   });
 });
