46c9a0b474517a4a60032f14e1864b9093902049
arjavsethi <75158301+arjavsethi@users.noreply.github.com>
2022-09-28 22:35:46 +0530

    Routing for sub-domains added

--- packages/web/pages/page/[slug].tsx
+++ packages/web/pages/page/[slug].tsx
@@ -1,49 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import { useGetResponses } from '@frontend/shared/hooks/response/getResponse';
 import { useGetForm } from '@frontend/shared/hooks/form/getForm';
-import parse from 'html-react-parser';
-const Slug = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  // search for appid in slug
-  const selector = useSelector((state) => state);
-  console.log(selector);
-  // console.log(selector.setting);
-  // const data = selector?.setting?.appForm?.fields.filter((item) => item?.form?.name === 'pages');
-  // const pageId = data?.[0]?.form?.name || '6324e600fe046781e9d33d6f'; //Form ID for form named pages "6324e600fe046781e9d33d6f"
 
-  const {
-    data: form,
-    error: formError,
-    loading: formLoading,
-  } = useGetForm('6324e600fe046781e9d33d6f');
-  console.log(form);
-  const slug_id = form?.getForm?.fields?.filter((item) => item?.label === 'slug');
-  const slugId = slug_id?.[0]?._id;
-  const { data, error, loading, state, setState } = useGetResponses({
-    formId: '6324e600fe046781e9d33d6f',
+import parse from 'html-react-parser';
+
+const PageId = (selector: any) => {
+  if (selector.setting.appForm) {
+    // console.log('appform', selector.setting.appForm);
+    const data1 = selector?.setting?.appForm?.fields.filter((item) => item?.form?.name === 'Pages');
+    const pageId = data1?.[0]?.form?._id; // || '6324e600fe046781e9d33d6f'; //Form ID for form named pages "6324e600fe046781e9d33d6f"
+    // console.log(pageId);
+    return pageId;
+  }
+};
+const slugFieldId = (pageId: string) => {
+  const { data: form, error: formError, loading: formLoading } = useGetForm(pageId);
+  // console.log(form);
+  const slug1 = form?.getForm?.fields?.filter((item) => item?.label === 'slug');
+  const slugId = slug1?.[0]?._id;
+  return slugId;
+};
+const htmlCode = (slugId: string, slug: string | string[], pageId) => {
+  // if (slugId && slug && pageId) {
+  const { data, error, loading } = useGetResponses({
+    formId: pageId, // || '6324e600fe046781e9d33d6f',
     valueFilter: {
       'values.field': slugId,
       'values.value': slug,
     },
   });
 
-  console.log(slugId);
-  console.log(data);
-  console.log('error' + error);
+  // console.log(slugId);
+  // console.log(data);
+  // console.log(`error${error}`);
   let html;
   if (data) {
-    let htmlCode = data?.getResponses?.data[0]?.values?.filter(
+    const code = data?.getResponses?.data[0]?.values?.filter(
       (item) => item?.field === '6324e6109ae5f7b99a8f5130',
     );
-    console.log(htmlCode);
-    html = htmlCode?.[0]?.value;
-    console.log(String(html));
+    // console.log(htmlCode);
+    html = code?.[0]?.value;
+    // console.log(String(html));
+    return String(html);
   }
+  // }
+};
+const Slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+  const selector = useSelector((state) => state);
+  // search for appid in slug
+  const pageId = PageId(selector);
+  const slugId = slugFieldId(pageId);
+  const html = htmlCode(slugId, slug, pageId);
 
-  return <div> {parse(String(html))}</div>;
+  // return <div> {parse(String(html))}</div>;
+  return <div> {String(html) !== 'undefined' ? parse(String(html)) : <>Loading</>}</div>;
 };
 
 export default Slug;
