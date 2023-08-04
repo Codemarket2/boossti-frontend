af83acf2ec8f9b2d8aa9e61fb6def93ed844d108
Arjav Sethi <75158301+arjavsethi@users.noreply.github.com>
2023-08-04 19:08:16 +0530

    template created for babessell homepage

--- packages/web/src/components/app/AppDashboard.tsx
+++ packages/web/src/components/app/AppDashboard.tsx
@@ -4,16 +4,16 @@ import parse from 'html-react-parser';
 import { useGetFormBySlug } from '@frontend/shared/hooks/form';
 import ErrorLoading from '../common/ErrorLoading';
 import NotFound from '../common/NotFound';
-import CloudHosting from '../scrumtop/CloudHosting';
+import Babessell from '../babessell';
 
 export default function AppDashboard() {
   const { appForm, appResponse } = useSelector((state: { setting: ISetting }) => state?.setting);
   const domain = window.location.host;
   if (
-    ['www.scrumtop.com', 'scrum.boossti.com'].includes(domain) ||
-    process.env.NEXT_PUBLIC_APP_DOMAIN === 'scrum.boossti.com'
+    ['www.babessell.com', 'scrum.boossti.com'].includes(domain) ||
+    process.env.NEXT_PUBLIC_APP_DOMAIN === 'babessell.boossti.com'
   ) {
-    return <CloudHosting />;
+    return <Babessell />;
   }
 
   const homepageFieldId = appForm?.fields?.find((field) => field?.label === 'Homepage')?._id;
