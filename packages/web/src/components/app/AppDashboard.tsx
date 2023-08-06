efd826dd28cd98b68029cc2f076062681eb59b4f
Arjav Sethi <75158301+arjavsethi@users.noreply.github.com>
2023-08-06 10:41:55 +0530

    Scrum top added

--- packages/web/src/components/app/AppDashboard.tsx
+++ packages/web/src/components/app/AppDashboard.tsx
@@ -5,21 +5,27 @@ import { useGetFormBySlug } from '@frontend/shared/hooks/form';
 import ErrorLoading from '../common/ErrorLoading';
 import NotFound from '../common/NotFound';
 import Babessell from '../babessell';
-
+import ScrumTop from '../scrumtop';
 export default function AppDashboard() {
   const { appForm, appResponse } = useSelector((state: { setting: ISetting }) => state?.setting);
   const domain = window.location.host;
   if (
-    ['www.babessell.com', 'scrum.boossti.com'].includes(domain) ||
+    ['www.babessell.com', 'babessell.boossti.com'].includes(domain) ||
     process.env.NEXT_PUBLIC_APP_DOMAIN === 'babessell.boossti.com'
   ) {
     return <Babessell />;
   }
-
+  if (
+    ['www.scrumtop.com', 'scrum.boossti.com'].includes(domain) ||
+    process.env.NEXT_PUBLIC_APP_DOMAIN === 'scrum.boossti.com'
+  ) {
+    return <ScrumTop />;
+  }
   const homepageFieldId = appForm?.fields?.find((field) => field?.label === 'Homepage')?._id;
 
-  const homepageResponse = appResponse?.values?.find((value) => value?.field === homepageFieldId)
-    ?.response;
+  const homepageResponse = appResponse?.values?.find(
+    (value) => value?.field === homepageFieldId,
+  )?.response;
 
   const { data: pagesForm, error, loading } = useGetFormBySlug('pages');
 
