73909d3525de711c3bead557d6f253d121ae1fa6
Santhanam-san <ranaranran@gmail.com>
2023-12-18 01:18:34 +0530

    Updated branch:Santhanam

--- packages/web/pages/thinking/Thinking.tsx
+++ packages/web/pages/thinking/Thinking.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-// import ResponsiveGridLayout, { WidthProvider, Responsive } from 'react-grid-layout';
-
 import PropTypes from 'prop-types';
 import ResponsiveGridLayout from 'react-grid-layout';
 
@@ -11,30 +9,23 @@ const layout = [
 ];
 
 function FilterableProductTable({ products }) {
-  const [filterText, setFilterText] = React.useState('');
-  const [inStockOnly, setInStockOnly] = React.useState(false);
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
 
   return (
-    // <ResponsiveGridLayout
-    //   className="layout"
-    //   layout={layout}
-    //   cols={12}
-    //   rowHeight={30}
-    //   width={1200}
-    //   isDraggable={true}
-    //   allowOverlap={true}
-    // >
-    <div>
-      <SearchBar
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-        onFilterTextChange={setFilterText}
-        onInStockOnlyChange={setInStockOnly}
-      />
-
-      <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
-    </div>
-    // </ResponsiveGridLayout>
+    <ResponsiveGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+      <div key="a" style={{ backgroundColor: 'white' }}>
+        <SearchBar
+          filterText={filterText}
+          inStockOnly={inStockOnly}
+          onFilterTextChange={setFilterText}
+          onInStockOnlyChange={setInStockOnly}
+        />
+      </div>
+      <div key="b" style={{ backgroundColor: 'white' }}>
+        <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
+      </div>
+    </ResponsiveGridLayout>
   );
 }
 
@@ -51,21 +42,11 @@ FilterableProductTable.propTypes = {
 
 function ProductCategoryRow({ category }) {
   return (
-    // <ResponsiveGridLayout
-    //   className="layout"
-    //   layout={layout}
-    //   cols={12}
-    //   rowHeight={30}
-    //   width={1200}
-    //   isDraggable={true}
-    //   allowOverlap={true}
-    // >
-    <div key="b" style={{ backgroundColor: 'red' }}>
+    <div style={{ backgroundColor: 'red' }}>
       <tr>
         <th colSpan={2}>{category}</th>
       </tr>
     </div>
-    // </ResponsiveGridLayout>
   );
 }
 
@@ -81,22 +62,10 @@ function ProductRow({ product }) {
   );
 
   return (
-    // <ResponsiveGridLayout
-    //   className="layout"
-    //   layout={layout}
-    //   cols={12}
-    //   rowHeight={30}
-    //   width={1200}
-    //   isDraggable={true}
-    //   allowOverlap={true}
-    // >
-    // <div key="b" style={{ backgroundColor: 'white' }}>
     <tr>
       <td>{name}</td>
       <td>{product.price}</td>
     </tr>
-    // </div>
-    // // </ResponsiveGridLayout>
   );
 }
 
@@ -127,27 +96,17 @@ function ProductTable({ products, filterText, inStockOnly }) {
   });
 
   return (
-    <ResponsiveGridLayout
-      className="layout"
-      layout={layout}
-      cols={12}
-      rowHeight={30}
-      width={1200}
-      isDraggable
-      allowOverlap
-    >
-      <div key="b" style={{ backgroundColor: 'white' }}>
-        <table>
-          <thead>
-            <tr>
-              <th>Name &nbsp; &nbsp; &nbsp;</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
-      </div>
-    </ResponsiveGridLayout>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name &nbsp; &nbsp; &nbsp;</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
   );
 }
 
@@ -166,34 +125,22 @@ ProductTable.propTypes = {
 
 function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
   return (
-    <ResponsiveGridLayout
-      className="layout"
-      layout={layout}
-      cols={12}
-      rowHeight={30}
-      width={1200}
-      isDraggable
-      allowOverlap
-    >
-      <div key="b" style={{ backgroundColor: 'blue' }}>
-        <form>
-          <input
-            type="text"
-            value={filterText}
-            placeholder="Search..."
-            onChange={(e) => onFilterTextChange(e.target.value)}
-          />
-          <label htmlFor="filterTextInput">
-            <input
-              type="checkbox"
-              checked={inStockOnly}
-              onChange={(e) => onInStockOnlyChange(e.target.checked)}
-            />{' '}
-            Only show products in stock
-          </label>
-        </form>
-      </div>
-    </ResponsiveGridLayout>
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+      <label htmlFor="filterTextInput">
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
+        />{' '}
+        Only show products in stock
+      </label>
+    </form>
   );
 }
 
@@ -216,199 +163,3 @@ const PRODUCTS = [
 export default function Thinking() {
   return <FilterableProductTable products={PRODUCTS} />;
 }
-
-// const initialState = {
-//   layouts: {},
-//   styles: {},
-//   selectedField: null,
-//   selectedElement: null,
-//   layoutEdit: true,
-//   editMode: false,
-// };
-
-// const layout = [
-//   { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
-//   { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-//   { i: 'c', x: 4, y: 0, w: 1, h: 2 },
-// ];
-
-// function FilterableProductTable({ products }) {
-//   const [filterText, setFilterText] = useState('');
-//   const [inStockOnly, setInStockOnly] = useState(false);
-
-//   return (
-//     // <>
-//     //   <ResponsiveGridLayout
-//     //     className="layout"
-//     //     layout={layout}
-//     //     cols={12}
-//     //     rowHeight={30}
-//     //     width={1200}
-//     //   >
-//     //     <div key="b" style={{ backgroundColor: 'violet' }}>
-//     //       <SearchBar
-//     //         filterText={filterText}
-//     //         inStockOnly={inStockOnly}
-//     //         onFilterTextChange={setFilterText}
-//     //         onInStockOnlyChange={setInStockOnly}
-//     //       />
-//     //       <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
-//     //     </div>
-//     //   </ResponsiveGridLayout>
-//     // </>
-//     <div>
-//       <SearchBar
-//         filterText={filterText}
-//         inStockOnly={inStockOnly}
-//         onFilterTextChange={setFilterText}
-//         onInStockOnlyChange={setInStockOnly}
-//       />
-//       <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
-//     </div>
-//   );
-// }
-
-// function ProductCategoryRow({ category }) {
-//   return (
-//     <>
-//       <ResponsiveGridLayout
-//         className="layout"
-//         layout={layout}
-//         cols={12}
-//         rowHeight={30}
-//         width={1200}
-//       >
-//         <div key="b" style={{ backgroundColor: 'red' }}>
-//           <tr>
-//             <th colSpan={2}>{category}</th>
-//           </tr>
-//         </div>
-//       </ResponsiveGridLayout>
-//     </>
-//   );
-// }
-
-// function ProductRow({ product }) {
-//   const name = product.stocked ? (
-//     product.name
-//   ) : (
-//     <span style={{ color: 'red' }}>{product.name}</span>
-//   );
-
-//   return (
-//     <>
-//       <ResponsiveGridLayout
-//         className="layout"
-//         layout={layout}
-//         cols={12}
-//         rowHeight={30}
-//         width={1200}
-//       >
-//         <div key="b" style={{ backgroundColor: 'white' }}>
-//           <tr>
-//             <td>{name}</td>
-//             <td>{product.price}</td>
-//           </tr>
-//         </div>
-//       </ResponsiveGridLayout>
-//     </>
-//   );
-// }
-
-// function ProductTable({ products, filterText, inStockOnly }) {
-//   const rows = [];
-//   let lastCategory = null;
-
-//   products.forEach((product) => {
-//     if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
-//       return;
-//     }
-//     if (inStockOnly && !product.stocked) {
-//       return;
-//     }
-//     if (product.category !== lastCategory) {
-//       rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-//     }
-//     rows.push(<ProductRow product={product} key={product.name} />);
-//     lastCategory = product.category;
-//   });
-
-//   return (
-//     // <>
-//     //   <ResponsiveGridLayout
-//     //     className="layout"
-//     //     layout={layout}
-//     //     cols={12}
-//     //     rowHeight={30}
-//     //     width={1200}
-//     //   >
-//     //     <div key="b" style={{ backgroundColor: 'violet' }}>
-//     //       <table>
-//     //         <thead>
-//     //           <tr>
-//     //             <th>Name</th>
-//     //             <th>Price</th>
-//     //           </tr>
-//     //         </thead>
-//     //         <tbody>{rows}</tbody>
-//     //       </table>
-//     //     </div>
-//     //   </ResponsiveGridLayout>
-//     // </>
-//     <table>
-//       <thead>
-//         <tr>
-//           <th>Name &nbsp; &nbsp; &nbsp;</th>
-//           <th>Price</th>
-//         </tr>
-//       </thead>
-//       <tbody>{rows}</tbody>
-//     </table>
-//   );
-// }
-
-// function SearchBar({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) {
-//   return (
-//     <>
-//       <ResponsiveGridLayout
-//         className="layout"
-//         layout={layout}
-//         cols={12}
-//         rowHeight={30}
-//         width={1200}
-//       >
-//         <div key="b" style={{ backgroundColor: 'blue' }}>
-//           <form>
-//             <input
-//               type="text"
-//               value={filterText}
-//               placeholder="Search..."
-//               onChange={(e) => onFilterTextChange(e.target.value)}
-//             />
-//             <label>
-//               <input
-//                 type="checkbox"
-//                 checked={inStockOnly}
-//                 onChange={(e) => onInStockOnlyChange(e.target.checked)}
-//               />{' '}
-//               Only show products in stock
-//             </label>
-//           </form>
-//         </div>
-//       </ResponsiveGridLayout>
-//     </>
-//   );
-// }
-
-// const PRODUCTS = [
-//   { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
-//   { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
-//   { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
-//   { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
-//   { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
-//   { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
-// ];
-
-// export default function Thinking() {
-//   return <FilterableProductTable products={PRODUCTS} />;
-// }
